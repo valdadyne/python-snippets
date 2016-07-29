0c80e0bae5e049e022fbfd49c4cd963b2f2f41e4
valdadyne <rodgers.gitau@gmail.com>
2016-07-29 12:10:48 +0300

    Updated

--- Getting Started with JSON Files/Data_Count/file.py
+++ Getting Started with JSON Files/Data_Count/file.py
@@ -1,6 +1,7 @@
-'''
+"""
 This is a Python Snippet that filters data in a JSON file based on the gender the user inputs
-'''
+It then outputs the number per each gender
+"""
 
 import json
 
@@ -11,6 +12,11 @@ if user_input!="":
         data=json.load(json_data)
         for r in (data['Users']):
             if (r['gender'] == user_input):
+                count=0
                 print (r['id'],str(r['first_name']),str(r['last_name']),str(r['email']))
+                for r['id']in r:
+                    count+=len(r)
+        print '\n'
+        print 'The Number of '+user_input+' is',count
 else:
     print("You must enter a gender!!")
